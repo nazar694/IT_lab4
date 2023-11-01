1f79dc85b8af117b75f912eaf3ff6b04e538ae2d
nazar694 <nazikmail124@gmil.com>
2023-11-01 17:58:16 +0200

    TEST

--- tests.py
+++ tests.py
@@ -4,27 +4,23 @@ import numpy as np
 
 
 def test_correct():
-    # Ввід початкових даних
-    supply = np.array([10, 20, 30])  # Постачальники
-    demand = np.array([16, 12, 11, 10])  # Споживачі
+    supply = np.array([10, 20, 30])
+    demand = np.array([16, 12, 11, 10])
     costs = np.array([[17, 16, 15, 14],
                       [12, 13, 11, 12],
-                      [15, 18, 19, 17]])  # Вартість перевезення
+                      [15, 18, 19, 17]])
     routes = transport_min_cost(supply, demand, costs)
-    # Вартість доставки
     total = np.sum(routes * costs)
     assert total == 690
 
 
 def test_failed():
-    # Ввід початкових даних
-    supply = np.array([15, 25, 35])  # Постачальники
-    demand = np.array([16, 12, 11, 10])  # Споживачі
+    supply = np.array([15, 25, 35])
+    demand = np.array([16, 12, 11, 10])
     costs = np.array([[17, 16, 15, 14],
                       [12, 13, 11, 12],
-                      [15, 18, 19, 17]])  # Вартість перевезення
+                      [15, 18, 19, 17]])
     routes = transport_min_cost(supply, demand, costs)
-    # Вартість доставки
     total = np.sum(routes * costs)
     assert total == 665
 
